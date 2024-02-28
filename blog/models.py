b7a31f578515d4788d1cc8bffb66b94d390b67a5
vrittech-manoj <manoj@vrittechnologies.com>
2024-02-28 21:23:07 +0545

    blog slug remove

--- blog/models.py
+++ blog/models.py
@@ -13,8 +13,3 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
     
-    def save(self, *args, **kwargs):
-        # Generate the slug when saving the holiday type if it's blanks
-        if not self.slug:
-            self.slug = slugify(self.name)+'-'+str(self.public_id)[1:5] + str(self.public_id)[-1:-5]
-        super().save(*args, **kwargs)
